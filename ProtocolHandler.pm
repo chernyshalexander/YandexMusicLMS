0056d276c54985d572bd0ea0d2a5b317e71fea50
chernyshalexander <chernysh.a@gmail.com>
2025-12-05 08:16:21 +0300

    kinda working ProtocolHandler

--- ProtocolHandler.pm
+++ ProtocolHandler.pm
@@ -3,129 +3,97 @@ package Plugins::yandex::ProtocolHandler;
 use strict;
 use warnings;
 use Slim::Utils::Log;
-use Slim::Utils::Prefs;
+use Slim::Player::Protocols::HTTPS;
+use JSON::XS::VersionOneAndTwo;
 
-# 1. ОБЯЗАТЕЛЬНО: Подгружаем и наследуем HTTP.
-# Это нужно, чтобы когда мы подменим ссылку на https, LMS знал, как её играть.
-require Slim::Player::Protocols::HTTP;
-use base qw(Slim::Player::Protocols::HTTP);
+# !!! ГЛАВНОЕ ИСПРАВЛЕНИЕ: Явно загружаем наш модуль !!!
+require Plugins::yandex::Track;
 
 my $log = logger('plugin.yandex');
 
-# --- Свойства ---
+# --- 1. КОНСТРУКТОР (как в Qobuz) ---
+sub new {
+    my $class  = shift;
+    my $args   = shift;
 
-sub isRemote { 1 }     
-sub isAudio { 1 }      
-sub audioScanned { 1 } 
-sub contentType { 'mp3' }
-sub getFormatForURL { 'mp3' }
-sub canSeek { 1 }
+    my $client    = $args->{client};
+    my $song      = $args->{song};
+    
+    # Берем URL, который должен быть уже установлен в getNextTrack
+    my $streamUrl = $song->streamUrl() || return;
 
-# --- 2. СКАНИРОВАНИЕ (Заглушка) ---
-# LMS вызывает это ПЕРЕД проигрыванием.
-# Если мы тут попытаемся сделать HTTP запрос к yandexmusic://, все упадет.
-# Поэтому просто возвращаем фиктивные данные.
+    $log->error("YANDEX: Handler new() called for REAL streamUrl: $streamUrl");
+
+    my $sock = $class->SUPER::new( {
+        url     => $streamUrl,
+        song    => $song,
+        client  => $client,
+    } ) || return;
+
+    # Устанавливаем тип контента, как в Deezer
+    ${*$sock}{contentType} = 'audio/mpeg';
+
+    return $sock;
+}
+
+# --- 2. isDirect (как в Qobuz) ---
+sub isDirect { 1 }
+
+# --- 3. scanUrl (как в Qobuz и Deezer) ---
 sub scanUrl {
     my ($class, $url, $args) = @_;
-    $log->info("YANDEX: scanUrl request: $url");
-    
-    return {
-        type    => 'mp3',
-        bitrate => 192000, # Говорим LMS, что это mp3 192kbps
-        length  => 0,      # Длительность пока неизвестна
-    };
+    $args->{cb}->( $args->{song}->currentTrack() );
 }
 
-# --- 3. МЕТАДАННЫЕ ---
-# Чтобы на экране было красиво
-sub getMetadata {
-    my ($class, $client, $url) = @_;
-
-    my $id = $url;
-    $id =~ s/^yandexmusic:\/\///;
-    $id =~ s/^track\///;
-
-    my $meta = {
-        title    => "Yandex Track $id",
-        artist   => "Yandex Music",
-        type     => 'mp3',
-        bitrate  => '192k',
-    };
-    
-    # Быстро достаем инфу из кэша клиента (если есть)
-    if (defined $Plugins::yandex::Plugin::ymClient) {
-        eval {
-             # Здесь можно использовать быстрый метод получения инфы
-             # Но главное, чтобы он не вешал интерфейс
-             my $info = $Plugins::yandex::Plugin::ymClient->get_tracks_info([$id]);
-             if ($info && $info->[0]) {
-                 my $t = $info->[0];
-                 $meta->{title} = $t->{title};
-                 $meta->{artist} = $t->{artists}->[0]->{name};
-                 $meta->{duration} = int($t->{durationMs} / 1000);
-                 if ($t->{coverUri}) {
-                     my $c = $t->{coverUri}; $c =~ s/%%/200x200/;
-                     $meta->{cover} = 'https://' . $c;
-                 }
-             }
-        };
-    }
-    return $meta;
-}
-
-# --- 4. РЕЗОЛВИНГ (Самое важное) ---
-# Мы используем getNextTrack вместо getStreamUrl.
-# Задача этого метода: превратить yandexmusic://... в https://...
-# И сказать LMS: "Я всё сделал, теперь играй новую ссылку".
-
+# --- 4. getNextTrack (ПЕРЕПИСЫВАЕМ НА АСИНХРОННЫЙ ВЫЗОВ) ---
 sub getNextTrack {
     my ($class, $song, $successCb, $errorCb) = @_;
 
     my $url = $song->currentTrack()->url;
-    $log->info("YANDEX: getNextTrack called for: $url");
+    $log->error("YANDEX: getNextTrack called for: $url");
 
-    # 1. Парсим ID
     my $track_id = $url;
-    if ($url =~ /yandexmusic:\/\/(?:track\/)?(\d+)/) {
-        $track_id = $1;
-    } else {
-        $log->error("YANDEX: Can't parse ID");
-        $errorCb->(); return;
+    unless ($url =~ /yandexmusic:\/\/(?:track\/)?(\d+)/) {
+        $log->error("YANDEX: Can't parse ID from URL: $url");
+        $errorCb->('Invalid URL format');
+        return;
     }
+    $track_id = $1;
 
-    my $ym_client = $Plugins::yandex::Plugin::ymClient;
-    unless ($ym_client) {
-        $log->error("YANDEX: Client not ready");
-        $errorCb->(); return;
-    }
+    # Создаем объект трека. Теперь он должен создаться без ошибок.
+    my $track = Plugins::yandex::Track->new({ id => $track_id });
 
-    # 2. Получаем реальную ссылку
-    my $stream_url;
-    eval {
-        my $track = Plugins::yandex::Track->new({ id => $track_id });
-        $stream_url = $track->get_stream_url($ym_client);
-    };
+    # !!! ГЛАВНОЕ: Вызываем АСИНХРОННЫЙ метод из Track.pm !!!
+    $track->get_direct_url(sub {
+        my ($final_url, $error) = @_;
 
-    if ($stream_url) {
-        $log->info("YANDEX: URL resolved: $stream_url");
-        
-        # 3. ПОДМЕНА ССЫЛКИ!
-        # Мы заменяем виртуальную ссылку на прямую HTTPS
-        $song->streamUrl($stream_url);
-        
-        # 4. УСПЕХ
-        # Вызываем callback. LMS увидит, что ссылка в $song изменилась на https://
-        # И передаст управление стандартному модулю Slim::Player::Protocols::HTTP
-        $successCb->();
-    } else {
-        $log->error("YANDEX: Failed to get URL");
-        $errorCb->();
-    }
+        if ($final_url) {
+            $log->error("YANDEX: ASYNC URL resolved: $final_url");
+            
+            # Устанавливаем реальную ссылку в объект песни
+            $song->streamUrl($final_url);
+            
+            # Сообщаем об успехе
+            $successCb->();
+        } else {
+            $log->error("YANDEX: ASYNC URL resolution failed: $error");
+            # Сообщаем об ошибке
+            $errorCb->($error);
+        }
+    });
 }
 
-# getStreamUrl нам больше не нужен, удаляем его, чтобы не путать LMS.
+# --- 5. Остальные методы (без изменений) ---
+sub getFormatForURL { 'mp3' }
+sub isRemote { 1 }
+sub isAudio { 1 }
+sub canSeek { 1 }
 
-# --- Разрешения ---
+sub getMetadata {
+    my ($class, $client, $url) = @_;
+    # ... ваш код ...
+}
+
 sub canDoAction {
     my ($class, $client, $url, $action) = @_;
     return 1 if $action =~ /^(pause|stop|seek|rew|fwd)$/;
