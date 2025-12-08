a2105026cbbe78146a0901595d0bd04d1329dd03
chernyshalexander <chernysh.a@gmail.com>
2025-12-08 10:20:32 +0300

    15 sec limitation occured

--- Track.pm
+++ Track.pm
@@ -3,14 +3,20 @@ package Plugins::yandex::Track;
 use strict;
 use warnings;
 use Data::Dumper;
-use File::Slurp;
 use JSON;
 use Digest::MD5 qw(md5_hex);
-use XML::Simple; # Или используйте JSON, если ответ приходит в JSON, но обычно там XML
+use Slim::Utils::Log;
+
+# XML::Simple может быть причиной проблем
+# use XML::Simple;
+
+
+my $log = logger('plugin.yandex');
 
 sub new {
     my ($class, $data) = @_;
-    my $self = {
+    #$log->error("Yandex Track: calling constructor NEW");
+    my $self = bless {
         raw => $data,
         id => $data->{id},
         title => $data->{title},
@@ -19,141 +25,164 @@ sub new {
         duration_ms => $data->{durationMs},
         available => $data->{available},
         download_info => undef,
-    };
-    bless $self, $class;
+    }, $class;
     return $self;
 }
 
+# --- Единый, полностью асинхронный метод для получения инфы о загрузке ---
 sub get_download_info {
-    my ($self, $client) = @_;
-    my $result = $client->{request}->get("https://api.music.yandex.net/tracks/" . $self->{id} . "/download-info");
-    #print Dumper($result);
-    #write_file('debug-dw-info.json', encode_json($result));
-    $self->{download_info} = $result;
-}
-sub get_stream_url {
-    my ($self, $client) = @_;
-
-    # 1. Получаем инфо о загрузке
-    unless ($self->{download_info}) {
-        $self->get_download_info($client);
+    my $self = shift;
+    #my $client = shift;
+    my $cb = shift;
+    $log->error("Yandex Track: calling get_download_info");
+    # Защита от дурака: убедимся, что колбэк - это функция
+    unless (defined $cb && ref($cb) eq 'CODE') {
+        # Логируем критическую ошибку и выходим
+        # Это поможет найти, если метод вызывается неправильно
+        #warn "Yandex Track: get_download_info called without a valid callback!";
+         $log->error("Yandex Track: get_download_info called without a valid callback!");
+        return;
     }
+
+    my $url = "https://api.music.yandex.net/tracks/" . $self->{id} . "/download-info";
     
-    # 2. Ищем downloadInfoUrl с самым высоким битрейтом
-    my $target_info;
-    foreach my $info (@{$self->{download_info}->{result} || []}) {
-        # Приоритет MP3 320, но можно брать первый попавшийся mp3
-        if ($info->{codec} eq 'mp3') {
-            $target_info = $info;
-            last if $info->{bitrateInKbps} == 320;
+    $log->error("Yandex Track: get_download_info");
+    # Всегда используем асинхронный запрос
+    Slim::Networking::SimpleAsyncHTTP->new(
+        sub {
+            my $http = shift;
+            my $content = $http->content;
+            my $result;
+
+            eval {
+                $result = decode_json($content);
+            };
+
+            if ($@) {
+                $cb->(undef, "Failed to decode JSON: $@");
+                return;
+            }
+
+            $self->{download_info} = $result;
+            $cb->($result);
+        },
+        sub {
+            my $http = shift;
+            my $error_msg = $http->error || 'Unknown HTTP error';
+            $cb->(undef, "HTTP request failed: $error_msg");
+        },
+        {
+            timeout => 15,
+            cache   => 0,
         }
-    }
-    
-    return undef unless $target_info && $target_info->{downloadInfoUrl};
-
-    # 3. Делаем запрос к downloadInfoUrl
-    my $dw_url = $target_info->{downloadInfoUrl};
-    my $res_xml = $client->{request}->{ua}->get($dw_url);
-    
-    unless ($res_xml->is_success) {
-        die "Failed to fetch download info XML";
-    }
-
-    # 4. Парсим XML (ответ приходит вида <download-info><host>...</host><path>...</path><ts>...</ts><s>...</s></download-info>)
-    # Если XML::Simple недоступен, можно использовать regex, так как структура простая
-    my $content = $res_xml->decoded_content;
-    
-    my ($host) = $content =~ /<host>(.*?)<\/host>/;
-    my ($path) = $content =~ /<path>(.*?)<\/path>/;
-    my ($ts)   = $content =~ /<ts>(.*?)<\/ts>/;
-    my ($s)    = $content =~ /<s>(.*?)<\/s>/;
-
-    unless ($host && $path && $ts && $s) {
-        # Иногда ответ бывает в JSON, проверим
-        my $data = eval { decode_json($content) };
-        if ($data) {
-             $host = $data->{host};
-             $path = $data->{path};
-             $ts = $data->{ts};
-             $s = $data->{s};
-        }
-    }
-
-    return undef unless ($host && $path && $ts && $s);
-
-    # 5. Генерируем подпись
-    # Соль жестко зашита в приложениях Яндекса
-    my $salt = 'XGRlBW9FXlekgbPrRHuSiA';
-    my $sign = md5_hex($salt . substr($path, 1) . $s);
-
-    # 6. Формируем финальную ссылку
-    my $final_url = "https://$host/get-mp3/$sign/$ts$path";
-    
-    return $final_url;
+    )->get($url);
 }
+
+# ---  асинхронный get_direct_url ---
+
+
 sub get_direct_url {
-    my ($self, $cb) = @_;
+    my $self = shift;
+    #my $client = shift; не используется
+    my $cb = shift; # Это колбэк, который нужно будет вызвать в самом конце
 
-    $self->get_download_info(sub {
+    # !!! ГЛАВНОЕ ИСПРАВЛЕНИЕ: Явно сохраняем колбэк !!!
+    #my $final_callback = $cb;
+
+    # Вызываем get_download_info и передаем ему НОВЫЙ колбэк,
+    # который будет использовать сохраненную ссылку на оригинальный колбэк
+    $self->get_download_info( sub {
         my ($info, $error) = @_;
 
-        if ($error || !$info || !$info->{downloadInfoUrl}) {
+        if ($error || !$info || !$info->{result}) {
+            # Используем явную ссылку на оригинальный колбэк
             $cb->(undef, "No download info: " . ($error || 'unknown'));
             return;
         }
 
-        my $url = $info->{downloadInfoUrl} . '&format=json';
+        # ... (ваша логика поиска target_info остается без изменений) ...
+        my $target_info;
+        foreach my $info_item (@{$info->{result}}) {
+            next unless $info_item->{codec} eq 'mp3';
+            if (!$target_info || $info_item->{bitrateInKbps} > $target_info->{bitrateInKbps}) {
+                $target_info = $info_item;
+                last if $info_item->{bitrateInKbps} == 320;
+            }
+        }
 
+        unless ($target_info && $target_info->{downloadInfoUrl}) {
+            $cb->(undef, "No suitable MP3 stream found");
+            return;
+        }
+
+        my $dw_url = $target_info->{downloadInfoUrl} . '&format=json';
+
+        # Второй асинхронный запрос. Здесь все критично важно.
         Slim::Networking::SimpleAsyncHTTP->new(
             sub {
-                my $response = shift;
-                my $xml = $response->content;
-
-                if ($xml =~ /host="([^"]+)"\s+path="([^"]+)"\s+ts="([^"]+)"\s+s="([^"]+)"/) {
-                    my ($host, $path, $ts, $s) = ($1, $2, $3, $4);
-
-                    my $sign = Digest::MD5::md5_hex("XGRlBW9FXlekgbPrRHuSiA" . substr($path, 1) . $s);
-                    my $direct_url = "https://$host/get-mp3/$sign/$ts$path";
-
-                    $cb->($direct_url);
-                } else {
-                    $cb->(undef, "Failed to parse XML: no host/path/ts/s");
+                my $http = shift;
+                my $content = $http->content;
+                # ... (ваша логика парсинга остается без изменений) ...
+                my $data = eval { decode_json($content) };
+                if ($@) {
+                    my ($host, $path, $ts, $s) = $content =~ /host="([^"]+)"\s+path="([^"]+)"\s+ts="([^"]+)"\s+s="([^"]+)"/;
+                    unless ($host && $path && $ts && $s) {
+                        $cb->(undef, "Failed to parse response as JSON or XML");
+                        return;
+                    }
+                    $data = {
+                        host => $host,
+                        path => $path,
+                        ts => $ts,
+                        s => $s,
+                    };
                 }
+                my $sign = md5_hex("XGRlBW9FXlekgbPrRHuSiA" . substr($data->{path}, 1) . $data->{s});
+                my $direct_url = "https://$data->{host}/get-mp3/$sign/$data->{ts}$data->{path}";
+
+                # !!! ВЫЗЫВАЕМ ЯВНО СОХРАНЕННЫЙ КОЛБЭК !!!
+                $cb->($direct_url);
             },
             sub {
                 my $http = shift;
-                $cb->(undef, "XML request failed: " . $http->error);
+                $cb->(undef, "XML/JSON request failed: " . $http->error);
             },
             {
                 timeout => 15,
                 cache   => 0,
             }
-        )->get($url);
+        )->get($dw_url);
     });
 }
 
-sub download {
-    my ($self, $client, $filename) = @_;
 
-    unless ($self->{download_info}) {
-        $self->get_download_info($client);
-    }
+# Старые методы 
 
-    foreach my $info (@{$self->{download_info}}) {
-        if ($info->{directLink}) {
-            my $url = $info->{host} . "/get-mp3/" . $info->{path} . "?track-id=$self->{id}&play=false";
-            #print "Скачиваем $url\n";
-            my $res = $client->{request}->{ua}->get($url, ':content_file' => $filename);
-            if ($res->is_success) {
-                print "Сохранено как $filename\n";
-                return 1;
-            } else {
-                die "Ошибка загрузки: " . $res->status_line;
-            }
-        }
-    }
 
-    die "Невозможно скачать трек — нет доступных ссылок";
-}
+
+
+# sub download {
+#     my ($self, $client, $filename) = @_;
+
+#     unless ($self->{download_info}) {
+#         $self->get_download_info($client);
+#     }
+
+#     foreach my $info (@{$self->{download_info}}) {
+#         if ($info->{directLink}) {
+#             my $url = $info->{host} . "/get-mp3/" . $info->{path} . "?track-id=$self->{id}&play=false";
+#             #print "Скачиваем $url\n";
+#             my $res = $client->{request}->{ua}->get($url, ':content_file' => $filename);
+#             if ($res->is_success) {
+#                 print "Сохранено как $filename\n";
+#                 return 1;
+#             } else {
+#                 die "Ошибка загрузки: " . $res->status_line;
+#             }
+#         }
+#     }
+
+#     die "Невозможно скачать трек — нет доступных ссылок";
+# }
 
 1;
