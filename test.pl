40ed33ed2becb93590a800dad87a3879a0752c21
Alexander Chernysh <chernysh.a@gmail.com>
2026-04-27 15:36:48 +0300

    version bump

--- test.pl
+++
@@ -1,7 +0,0 @@
-for my $m ('<?xm', '<Err', '<!DO', '<htm', 'fLaC', 'ftyp') {
-    if ($m =~ /^(?:<\?xm|<err|<!do|<htm)/i) {
-        print "$m matches\n";
-    } else {
-        print "$m NO\n";
-    }
-}
